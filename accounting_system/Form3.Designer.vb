e02c4ce8cc3b452dddd7049dad8b200c4d2c273c
theos-the-dawg <motheo13@outlook.com>
2025-03-14 14:43:01 +0200

    add-changes

--- accounting_system/Form3.Designer.vb
+++ accounting_system/Form3.Designer.vb
@@ -22,6 +22,7 @@ Partial Class Form3
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
         group_a = New ComboBox()
         group_b = New ComboBox()
         Label1 = New Label()
@@ -29,6 +30,16 @@ Partial Class Form3
         additembtn = New Button()
         removeitembtn = New Button()
         user_selections = New ListBox()
+        Label3 = New Label()
+        ContextMenuStrip1 = New ContextMenuStrip(components)
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label4 = New Label()
+        Label5 = New Label()
+        TextBox3 = New TextBox()
+        Total_Pricetxt = New TextBox()
+        Label6 = New Label()
+        Display_btn = New Button()
         SuspendLayout()
         ' 
         ' group_a
@@ -91,14 +102,102 @@ Partial Class Form3
         user_selections.FormattingEnabled = True
         user_selections.Location = New Point(488, 57)
         user_selections.Name = "user_selections"
-        user_selections.Size = New Size(278, 344)
+        user_selections.Size = New Size(278, 104)
         user_selections.TabIndex = 6
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(359, 254)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(34, 20)
+        Label3.TabIndex = 7
+        Label3.Text = "VAT"
+        ' 
+        ' ContextMenuStrip1
+        ' 
+        ContextMenuStrip1.ImageScalingSize = New Size(20, 20)
+        ContextMenuStrip1.Name = "ContextMenuStrip1"
+        ContextMenuStrip1.Size = New Size(61, 4)
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(488, 247)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 9
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(488, 298)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 10
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(359, 298)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(67, 20)
+        Label4.TabIndex = 11
+        Label4.Text = "Discount"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(355, 359)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(127, 20)
+        Label5.TabIndex = 12
+        Label5.Text = "Promotional Price"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(488, 356)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(125, 27)
+        TextBox3.TabIndex = 13
+        ' 
+        ' Total_Pricetxt
+        ' 
+        Total_Pricetxt.Location = New Point(488, 389)
+        Total_Pricetxt.Name = "Total_Pricetxt"
+        Total_Pricetxt.Size = New Size(125, 27)
+        Total_Pricetxt.TabIndex = 14
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(359, 392)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(78, 20)
+        Label6.TabIndex = 15
+        Label6.Text = "Total Price"
+        ' 
+        ' Display_btn
+        ' 
+        Display_btn.Location = New Point(35, 392)
+        Display_btn.Name = "Display_btn"
+        Display_btn.Size = New Size(128, 29)
+        Display_btn.TabIndex = 16
+        Display_btn.Text = "Display"
+        Display_btn.UseVisualStyleBackColor = True
         ' 
         ' Form3
         ' 
         AutoScaleDimensions = New SizeF(8F, 20F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Display_btn)
+        Controls.Add(Label6)
+        Controls.Add(Total_Pricetxt)
+        Controls.Add(TextBox3)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label3)
         Controls.Add(user_selections)
         Controls.Add(removeitembtn)
         Controls.Add(additembtn)
@@ -119,4 +218,14 @@ Partial Class Form3
     Friend WithEvents additembtn As Button
     Friend WithEvents removeitembtn As Button
     Friend WithEvents user_selections As ListBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ContextMenuStrip1 As ContextMenuStrip
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Total_Pricetxt As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Display_btn As Button
 End Class
--- accounting_system/Form3.vb
+++ accounting_system/Form3.vb
@@ -1,11 +1,10 @@
-﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
-Imports System.Collections.Generic
-
-Public Class Form3
+﻿Public Class Form3
     Dim selectedItems As New List(Of String)
-    'The logic for addition of items is done from the 
+    Dim vat_percent As Decimal = 15.5
+    Dim groupASelected As Boolean = False
 
     Private Sub group_a_SelectedIndexChanged(sender As Object, e As EventArgs) Handles group_a.SelectedIndexChanged
+        groupASelected = True
 
         If Not selectedItems.Contains(group_a.SelectedItem.ToString()) Then
             selectedItems.Add(group_a.SelectedItem.ToString())
@@ -14,22 +13,24 @@ Public Class Form3
         ' Clear the ListBox and add all items from the list
         user_selections.Items.Clear()
         user_selections.Items.AddRange(selectedItems.ToArray())
-
     End Sub
-    Private Sub group_b_SelectedIndexChanged(sender As Object, e As EventArgs) Handles group_b.SelectedIndexChanged
-        'changes is logic it must allow adding the same item on the list as many a needed
-        If Not selectedItems.Contains(group_b.SelectedItem.ToString()) Then
-            selectedItems.Add(group_b.SelectedItem.ToString())
-        End If
 
-        ' Clear the ListBox and add all items from the list
-        user_selections.Items.Clear()
-        user_selections.Items.AddRange(selectedItems.ToArray())
+    Private Sub group_b_SelectedIndexChanged(sender As Object, e As EventArgs) Handles group_b.SelectedIndexChanged
+        If groupASelected Then
+            selectedItems.Add(group_b.SelectedItem.ToString())
+
+            ' Clear the ListBox and add all items from the list
+            user_selections.Items.Clear()
+            user_selections.Items.AddRange(selectedItems.ToArray())
+        Else
+            MessageBox.Show("Please select an item from Group A first.")
+        End If
     End Sub
 
     Private Sub removeitembtn_Click(sender As Object, e As EventArgs) Handles removeitembtn.Click
         If user_selections.SelectedIndex <> -1 Then
             ' Remove the selected item
+            selectedItems.Remove(user_selections.SelectedItem.ToString())
             user_selections.Items.RemoveAt(user_selections.SelectedIndex)
         Else
             MessageBox.Show("Please select an item to remove.")
@@ -37,32 +38,71 @@ Public Class Form3
     End Sub
 
     Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-        'Prices for group a
+        ' Prices for group a
         Dim group_a_pricelist As New Dictionary(Of String, Double) From {
             {"Laptop - R7500.00", 7500.0},
             {"Phone - R5000.50", 5000.5},
             {"Tablet - R4000.00", 4000.0}
         }
-        'for looping through the dictionary values to 
         For Each vp As KeyValuePair(Of String, Double) In group_a_pricelist
             group_a.Items.Add(vp.Key)
         Next
 
-        'prices for group b 
-        Dim group_b_pricelist As New Dictionary(Of String, Double)() From {
+        ' Prices for group b
+        Dim group_b_pricelist As New Dictionary(Of String, Double) From {
             {"Desktop - R11500.50", 11500.5},
             {"Headphones - R5000.50", 5000.5},
             {"Console - R12000.00", 12000.0}
         }
         For Each vp As KeyValuePair(Of String, Double) In group_b_pricelist
             group_b.Items.Add(vp.Key)
+
+
+
         Next
-
-
     End Sub
 
     Private Sub additembtn_Click(sender As Object, e As EventArgs) Handles additembtn.Click
+        ' Add item logic here
 
+    End Sub
+
+    Private Sub user_selections_SelectedIndexChanged(sender As Object, e As EventArgs) Handles user_selections.SelectedIndexChanged
+        ' Logic for user selections
+    End Sub
+    Private Function CalculateTotal() As Decimal
+        Dim total As Decimal = 0
+        For Each item As String In selectedItems
+            Dim priceString As String = item.Split("-"c)(1).Trim().Replace("R", "")
+            Dim price As Decimal
+
+            If Decimal.TryParse(priceString, price) Then
+                total += price
+            End If
+        Next
+        Return total
+        'Dim total As Decimal = 0
+        'For Each item As String In selectedItems
+        '    Dim priceString As String = item.Split("-"c)(1).Trim().Replace("R", "")
+        '    Dim price As Decimal
+
+        '    'Dim price As Decimal = Decimal.Parse(priceString)
+
+
+
+
+        '    priceString = Decimal.TryParse(priceString, price)
+
+        '    priceString += total
+        '    End If
+        '    priceString += total
+
+        'Next
+        'Return total
+
+    End Function
+
+    Private Sub Display_btn_Click(sender As Object, e As EventArgs) Handles Display_btn.Click
+        Total_Pricetxt.Text = CalculateTotal().ToString("C")
     End Sub
 End Class
